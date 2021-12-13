11517c5bd90f9c1b005ecab2333b99aa5dec2496
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 09:54:53 +0200

    cors2

--- server.js
+++ server.js
@@ -6,30 +6,40 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-const whitelist = new RegExp(
-  '^((https?:\\/\\/)?.*?([\\w\\d-]*\\.[\\w\\d]+))($|\\/.*$)'
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://baguette-store-web.herokuapp.com',
+  'https://baguette-store.herokuapp.com'
+]; // list of allow domain
+
+app.use(
+  cors(
+    app.use(
+      cors({
+        credentials: true,
+        origin: (origin, callback) => {
+          if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error(`Origin: ${origin} is now allowed`));
+          }
+        }
+      })
+    )
+  )
 );
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (!whitelist.test(origin)) {
-      var msg =
-        'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-};
-
-app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/dist/baguette-store-web'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/baguette-store-web/index.html'));
+});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
 // Start the app by listening on the default Heroku port
