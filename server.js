c737543c3282d3d835ae1ca3895760adf40ceff5
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 11:16:37 +0200

    feat: no cors

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(
-  cors({
-    origin: 'https://baguette-store-web.herokuapp.com',
-    credentials: true
-  })
-);
 app.use(express.static(__dirname + '/dist/baguette-store-web'));
 
 app.get('/*', function (req, res) {
