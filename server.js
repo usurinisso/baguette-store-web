2cb0bfb62f37d0678bd358714dec5fd8f600d20a
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 10:03:58 +0200

    feat:

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://baguette-store-web.herokuapp.com',
-  'https://baguette-store.herokuapp.com',
-  'http://localhost:4200'
-]; // list of allow domain
-
-app.use(
-  cors(
-    app.use(
-      cors({
-        origin: (origin, callback) => {
-          if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error(`Origin: ${origin} is now allowed`));
-          }
-        }
-      })
-    )
-  )
-);
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://baguette-store-web.herokuapp.com',
+//   'https://baguette-store.herokuapp.com',
+//   'http://localhost:4200'
+// ]; // list of allow domain
+//
+// app.use(
+//   cors(
+//     app.use(
+//       cors({
+//         origin: (origin, callback) => {
+//           if (allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//           } else {
+//             callback(new Error(`Origin: ${origin} is now allowed`));
+//           }
+//         }
+//       })
+//     )
+//   )
+// );
 app.use(express.static(__dirname + '/dist/baguette-store-web'));
 
 app.get('/*', function (req, res) {
