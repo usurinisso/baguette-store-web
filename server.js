b89164e34baa68b8e23ea350fba553af99772307
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 11:00:43 +0200

    cors

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(cors());
 app.use(express.static(__dirname + '/dist/baguette-store-web'));
 
 app.get('/*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
   res.sendFile(path.join(__dirname + '/dist/baguette-store-web/index.html'));
 });
 
